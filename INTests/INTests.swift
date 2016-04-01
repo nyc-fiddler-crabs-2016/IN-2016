3a696d8de202b1c78687a9f170821876b167c880
Charlie Bliss <charles.m.bliss@gmail.com>
2016-04-01 17:48:13 -0400

    Initial Commit

---
+++ INTests/INTests.swift
@@ -0,0 +1,36 @@
+//
+//  INTests.swift
+//  INTests
+//
+//  Created by Apprentice on 4/1/16.
+//  Copyright © 2016 Zack Mance Octothorpe Production Company. All rights reserved.
+//
+
+import XCTest
+@testable import IN
+
+class INTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
